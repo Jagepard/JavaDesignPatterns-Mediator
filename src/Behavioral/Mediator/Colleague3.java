06718c1199d90f784be2a028696456e9f94f02d9
Jagepard <dankorot@gmail.com>
2020-04-16 12:05:06 +0300

    add AbstractListener

--- src/Behavioral/Mediator/Colleague3.java
+++ src/Behavioral/Mediator/Colleague3.java
@@ -7,17 +7,5 @@ package Behavioral.Mediator;
 
 import java.lang.reflect.InvocationTargetException;
 
-public class Colleague3 implements ListenerInterface {
-    public void onEvent(HandlerInterface handler)
-    {
-        handler.setMessage("Colleague3");
-        System.out.printf("%s%s\n", handler.getMessage(), ": notified");
-    }
-
-    public void todo(MediatorInterface mediator, HandlerInterface handler) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        System.out.printf("%s\n", "Colleague3: act");
-
-        mediator.notify("colleague_1", null);
-        mediator.notify("colleague_2", handler);
-    }
+public class Colleague3 extends AbstractListener {
 }
