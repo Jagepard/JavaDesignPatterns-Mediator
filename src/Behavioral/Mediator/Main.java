2b70ee34a7a9884b018cccd5b5b29e250c93ffde
Jagepard <dankorot@gmail.com>
2020-04-16 12:09:12 +0300

    update

--- src/Behavioral/Mediator/Main.java
+++ src/Behavioral/Mediator/Main.java
@@ -10,18 +10,17 @@ public class Main {
     public static void main(String[] args) throws Exception {
         HandlerInterface handler   = new Handler();
         MediatorInterface mediator = new Mediator();
-        Colleague1 colleague_1 = new Colleague1();
-        Colleague2 colleague_2 = new Colleague2();
-        Colleague3 colleague_3 = new Colleague3();
+        AbstractListener listener1 = new Colleague1();
+        AbstractListener listener2 = new Colleague2();
+        AbstractListener listener3 = new Colleague3();
 
         try {
-            mediator.addListener("colleague_1", colleague_1, "onEvent");
-            mediator.addListener("colleague_2", colleague_2, "onEvent");
-            mediator.addListener("colleague_3", colleague_3, "onEvent");
-
-            colleague_1.todo(mediator, handler);
-            colleague_2.todo(mediator, handler);
-            colleague_3.todo(mediator, handler);
+            mediator.addListener(listener1, "onEvent");
+            mediator.addListener(listener2, "onEvent");
+            mediator.addListener(listener3, "onEvent");
+            listener1.toGreet(listener2.getClass().getName(), mediator, handler);
+            listener2.toGreet(listener3.getClass().getName(), mediator, handler);
+            listener3.toGreet(listener1.getClass().getName(), mediator, handler);
         } catch (Exception e) {
             System.out.printf("Caught exception: %s \n", e.getLocalizedMessage());
         }
